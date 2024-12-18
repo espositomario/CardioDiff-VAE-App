2a7a1dacff64e41d8890f8e8999ee361aa5c832c
Mario Esposito <01.esposito.mario@gmail.com>
2024-12-18 12:42:48 +0100

    Add gene trend plotting and PDF download functionality; update requirements

--- 1_Clusters_Navigator.py
+++ 1_Clusters_Navigator.py
@@ -79,7 +79,6 @@ st.markdown("<h5 style='text-align: center;'>Explore genes in each cluster in te
                 
 
 #-------------------Clusters composition-------------------#
-
 with st.expander("Clusters categories Maps",  icon=":material/stacked_bar_chart:", expanded=True):
     st.markdown("<h3 style='text-align: center;'>Genes distribution among clusters by categories</h3>", unsafe_allow_html=True)
 
@@ -128,13 +127,19 @@ st.markdown(f"<h5 style='text-align: center;'>Select a cluster from the sidebar<
 st.markdown(f"<h3 style='text-align: center;'>Cluster {k} (n= {NUM_OF_GENES})</h3>", unsafe_allow_html=True)   
 
 #------------------------------------------Features distributions------------------------------------------#
-with st.expander("Features distributions",  icon=":material/bar_chart:",expanded=True):
 
+#------------------------------------------Features distributions------------------------------------------#
+with st.expander("Features distributions",  icon=":material/bar_chart:",expanded=True):
     st.markdown("<h3 style='text-align: center;'>Features distributions</h3>", unsafe_allow_html=True,help="..")
 
     C = st.columns(4, gap="small")
 
-    for i,feature in enumerate(['RNA', 'H3K4me3', 'H3K27ac', 'H3K27me3']):
+    # Create a PdfPages object to store all the plots
+    pdf_path = "/tmp/plots.pdf"
+    pdf_pages = PdfPages(pdf_path)
+
+    # Loop through the features and generate the plots
+    for i, feature in enumerate(['RNA', 'H3K4me3', 'H3K27ac', 'H3K27me3']):
         FILT_DF = DATA[[f"{feature}_{ct}" for ct in CT_LIST]].copy()
         VMIN, VMAX = FILT_DF.min().min(), FILT_DF.max().max()
         VMIN = FILT_DF.quantile([0.001]).min(axis=1)[0.001]
@@ -143,10 +148,31 @@ with st.expander("Features distributions",  icon=":material/bar_chart:",expanded
         FILT_DF = FILT_DF[DATA['Cluster'] == k]
         
         with C[i]:
-            fig, ax= plot_violin_box(FILT_DF, feature, CT_LIST, HM_COL_DICT, CT_COL_DICT, y_lab='Z-score' if i==0 else'',
+            fig, ax = plot_violin_box(FILT_DF, feature, CT_LIST, HM_COL_DICT, CT_COL_DICT, y_lab='Z-score' if i==0 else '',
                                         VMIN=VMIN, VMAX=VMAX)
 
+            # Display the plot in the Streamlit app
             st.pyplot(fig)
+
+            # Save the figure to the PDF
+            pdf_pages.savefig(fig)  # This adds the figure to the PDF file
+
+    # Close the PDF after adding all the plots
+    pdf_pages.close()
+
+    # Add a download button for the PDF
+    with open(pdf_path, "rb") as pdf_file:
+        st.download_button(
+            label="",
+            icon=":material/download:",
+            data=pdf_file,
+            file_name=f"C{k}_Feature_distributions.pdf",
+            mime="application/pdf"
+        )
+
+
+
+
 
 
 #------------------------------------------TSS and Categories------------------------------------------#
@@ -187,120 +213,6 @@ with st.expander("MetaGene plots",  icon=":material/area_chart:", expanded=True)
 
 
 
-
-def plot_gene_trend(DATA, SEL_GENES, CT_LIST, CT_COL_DICT, Y_LAB):
-    """
-    Plot trends for selected genes across conditions with arrow connections between average points.
-    
-    Parameters:
-        DATA (pd.DataFrame): DataFrame where rows are genes, columns are RNA counts.
-        SEL_GENES (list): List of gene names to plot.
-        CT_LIST (list): List of ordered conditions (e.g., ["ESC", "MES", "CP", "CM"]).
-        CT_COL_DICT (dict): Mapping of condition names to colors.
-        Y_LAB (str): Label for the Y-axis.
-
-    Returns:
-        Plotly figure with subplots for each gene's trend.
-    """
-    num_genes = len(SEL_GENES)
-    num_cols = 6  # Fixed number of columns
-    num_rows = math.ceil(num_genes / num_cols)  # Calculate number of rows based on the number of genes
-
-    # Create subplot figure
-    fig = make_subplots(
-        rows=num_rows, cols=num_cols,
-        subplot_titles=SEL_GENES,
-        horizontal_spacing=0.05, vertical_spacing=0.1,
-    )
-
-    for i, gene_name in enumerate(SEL_GENES):
-        # Extract gene data
-        gene_data = DATA.loc[gene_name]
-
-        # Extract condition (CT) and replicate (REP) information
-        CT = pd.Categorical(gene_data.index.str.extract(f"({'|'.join(CT_LIST)})")[0], categories=CT_LIST, ordered=True)
-        REP = gene_data.index.str.extract(r'(\d)')[0]
-        df = pd.DataFrame({Y_LAB: gene_data.values, 'CT': CT, 'REP': REP})
-
-        # Filter out invalid rows
-        df = df.dropna()
-
-        # Compute average values for arrows
-        avg_df = df.groupby('CT', observed=False)[Y_LAB].mean().reset_index().sort_values('CT')
-
-        # Determine subplot location
-        row = (i // num_cols) + 1
-        col = (i % num_cols) + 1
-
-        # Add scatter plot for individual points
-        for ct in CT_LIST:
-            ct_data = df[df['CT'] == ct]
-            fig.add_trace(
-                go.Scatter(
-                    x=ct_data['CT'],
-                    y=ct_data[Y_LAB],
-                    mode='markers',
-                    marker=dict(size=12, color=CT_COL_DICT[ct]),
-                    name=ct,
-                    showlegend=False,
-                    hovertemplate=f"{ct}"
-                ),
-                row=row, col=col
-            )
-
-        # Add arrows between average points
-        for j in range(len(avg_df) - 1):
-            fig.add_annotation(
-                x=avg_df.iloc[j + 1]["CT"],
-                y=avg_df.iloc[j + 1][Y_LAB],
-                ax=avg_df.iloc[j]["CT"],
-                ay=avg_df.iloc[j][Y_LAB],
-                xref=f"x{i + 1}",
-                yref=f"y{i + 1}",
-                axref=f"x{i + 1}",
-                ayref=f"y{i + 1}",
-                arrowhead=5,
-                arrowsize=2,
-                arrowwidth=1,
-                arrowcolor="grey",
-                showarrow=True,
-            )
-
-        # Update subplot axes
-        y_max = math.ceil(df[Y_LAB].max() * 1.1)  # Determine max value with padding
-        fig.update_xaxes(title_text="", row=row, col=col, showticklabels=False)
-        fig.update_yaxes(
-            title_text=Y_LAB if col == 1 else "",
-            row=row, col=col,
-            range=[0, y_max],  # Limit range from 0 to max
-            tickvals=[0, y_max],  # Display only 0 and max
-            ticktext=[0, y_max],
-        )
-
-        # Add a horizontal line at y_max
-        fig.add_shape(
-            type="line",
-            x0=0,
-            x1=len(CT_LIST),  # Covers the entire x-axis range
-            y0=y_max,
-            y1=y_max,
-            xref=f"x{i + 1}",
-            yref=f"y{i + 1}",
-            line=dict(color="grey", width=0.5),
-            row=row,
-            col=col,
-        )
-
-    # Update figure layout
-    fig.update_layout(
-        height=100+(200 * num_rows), width=300 * num_cols,
-        title_text=None,
-        showlegend=False,
-        plot_bgcolor="white",
-    )
-
-    return fig
-
 #--------------------------------------------------------------
 with st.expander("Expression of selected genes", icon=":material/timeline:", expanded=False):
     st.markdown("<h3 style='text-align: center;'>Expression of selected genes </h3>", unsafe_allow_html=True)
@@ -332,7 +244,8 @@ with st.expander("Expression of selected genes", icon=":material/timeline:", exp
         FPKM = SEL.filter(FPKM_features)
         fig = plot_gene_trend(np.log2(FPKM+1), SEL_GENES, CT_LIST, CT_COL_DICT, Y_LAB="log2(FPKM+1)")
         st.plotly_chart(fig, use_container_width=True)
-
+        
+        plotly_download_pdf(fig, file_name=f"C{k}_GeneTrends.pdf")
 
 #--------------------------------------------------------------
 with st.expander("Functional Term Enrichment Analysis", icon=":material/hdr_strong:", expanded=True):
--- utils/my_module.py
+++ utils/my_module.py
@@ -8,12 +8,16 @@ import os
 import re
 from plotly.graph_objects import Figure, Violin
 import plotly.graph_objects as go
+import io
+import plotly.io as pio
 #custom components
 from streamlit_pdf_viewer import pdf_viewer
 from streamlit_extras.bottom_container import bottom
 from plotly.subplots import make_subplots
 import math
 from matplotlib import cm, colors as mcolors
+from matplotlib.backends.backend_pdf import PdfPages
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 from pandas.api.types import (
@@ -453,7 +457,7 @@ def plot_violin_box(df, feature_group, ct_list, hm_col_dict, ct_col_dict, k=None
     
     # Box plot overlay
     sns.boxplot(
-        data=melted_df, x='CT', y='Value', palette=[ct_col_dict[ct] for ct in ct_list], 
+        data=melted_df, x='CT', y='Value', palette=[ct_col_dict[ct] for ct in ct_list], hue='CT',
         ax=ax, width=0.6, saturation=0.8,
         showcaps=False,
         showfliers=False,
@@ -464,7 +468,7 @@ def plot_violin_box(df, feature_group, ct_list, hm_col_dict, ct_col_dict, k=None
     )
     # Scatter points overlay (jittered)
     sns.stripplot(
-        data=melted_df, x='CT', y='Value', palette=[ct_col_dict[ct] for ct in ct_list], 
+        data=melted_df, x='CT', y='Value', palette=[ct_col_dict[ct] for ct in ct_list], hue='CT',
         ax=ax, jitter=True, size=1, alpha=0.5, linewidth=0.5, 
     )
 
@@ -556,6 +560,119 @@ def plot_frame(border_color="#FFFFFF"):
 )
 
 
+def plot_gene_trend(DATA, SEL_GENES, CT_LIST, CT_COL_DICT, Y_LAB):
+    """
+    Plot trends for selected genes across conditions with arrow connections between average points.
+    
+    Parameters:
+        DATA (pd.DataFrame): DataFrame where rows are genes, columns are RNA counts.
+        SEL_GENES (list): List of gene names to plot.
+        CT_LIST (list): List of ordered conditions (e.g., ["ESC", "MES", "CP", "CM"]).
+        CT_COL_DICT (dict): Mapping of condition names to colors.
+        Y_LAB (str): Label for the Y-axis.
+
+    Returns:
+        Plotly figure with subplots for each gene's trend.
+    """
+    num_genes = len(SEL_GENES)
+    num_cols = 6  # Fixed number of columns
+    num_rows = math.ceil(num_genes / num_cols)  # Calculate number of rows based on the number of genes
+
+    # Create subplot figure
+    fig = make_subplots(
+        rows=num_rows, cols=num_cols,
+        subplot_titles=SEL_GENES,
+        horizontal_spacing=0.05, vertical_spacing=0.1,
+    )
+
+    for i, gene_name in enumerate(SEL_GENES):
+        # Extract gene data
+        gene_data = DATA.loc[gene_name]
+
+        # Extract condition (CT) and replicate (REP) information
+        CT = pd.Categorical(gene_data.index.str.extract(f"({'|'.join(CT_LIST)})")[0], categories=CT_LIST, ordered=True)
+        REP = gene_data.index.str.extract(r'(\d)')[0]
+        df = pd.DataFrame({Y_LAB: gene_data.values, 'CT': CT, 'REP': REP})
+
+        # Filter out invalid rows
+        df = df.dropna()
+
+        # Compute average values for arrows
+        avg_df = df.groupby('CT', observed=False)[Y_LAB].mean().reset_index().sort_values('CT')
+
+        # Determine subplot location
+        row = (i // num_cols) + 1
+        col = (i % num_cols) + 1
+
+        # Add scatter plot for individual points
+        for ct in CT_LIST:
+            ct_data = df[df['CT'] == ct]
+            fig.add_trace(
+                go.Scatter(
+                    x=ct_data['CT'],
+                    y=ct_data[Y_LAB],
+                    mode='markers',
+                    marker=dict(size=12, color=CT_COL_DICT[ct]),
+                    name=ct,
+                    showlegend=False,
+                    hovertemplate=f"{ct}"
+                ),
+                row=row, col=col
+            )
+
+        # Add arrows between average points
+        for j in range(len(avg_df) - 1):
+            fig.add_annotation(
+                x=avg_df.iloc[j + 1]["CT"],
+                y=avg_df.iloc[j + 1][Y_LAB],
+                ax=avg_df.iloc[j]["CT"],
+                ay=avg_df.iloc[j][Y_LAB],
+                xref=f"x{i + 1}",
+                yref=f"y{i + 1}",
+                axref=f"x{i + 1}",
+                ayref=f"y{i + 1}",
+                arrowhead=5,
+                arrowsize=2,
+                arrowwidth=1,
+                arrowcolor="grey",
+                showarrow=True,
+            )
+
+        # Update subplot axes
+        y_max = math.ceil(df[Y_LAB].max() * 1.1)  # Determine max value with padding
+        fig.update_xaxes(title_text="", row=row, col=col, showticklabels=False)
+        fig.update_yaxes(
+            title_text=Y_LAB if col == 1 else "",
+            row=row, col=col,
+            range=[0, y_max],  # Limit range from 0 to max
+            tickvals=[0, y_max],  # Display only 0 and max
+            ticktext=[0, y_max],
+        )
+
+        # Add a horizontal line at y_max
+        fig.add_shape(
+            type="line",
+            x0=0,
+            x1=len(CT_LIST),  # Covers the entire x-axis range
+            y0=y_max,
+            y1=y_max,
+            xref=f"x{i + 1}",
+            yref=f"y{i + 1}",
+            line=dict(color="grey", width=0.5),
+            row=row,
+            col=col,
+        )
+
+    # Update figure layout
+    fig.update_layout(
+        height=100+(200 * num_rows), width=300 * num_cols,
+        title_text=None,
+        showlegend=False,
+        plot_bgcolor="white",
+    )
+
+    return fig
+
 
 def add_footer():
     """
@@ -592,3 +709,32 @@ def add_footer():
     """
     st.markdown(footer_content, unsafe_allow_html=True)
     
+
+
+def plotly_download_pdf(fig, file_name):
+    """
+    Generates a PDF from a Plotly figure and adds a download button in Streamlit.
+    
+    Args:
+        fig (plotly.graph_objects.Figure): Plotly figure to save to PDF.
+        pdf_path (str): Path where the PDF will be saved (default is "/tmp/plot.pdf").
+        button_label (str): Label for the download button (default is 'Download PDF').
+    """
+    # Make sure the output directory exists
+    pdf_path = f"/tmp/{file_name}"
+    os.makedirs(os.path.dirname(pdf_path), exist_ok=True)
+
+    # Save the Plotly figure to a PDF file
+    pio.write_image(fig, pdf_path, format='pdf')
+
+    # Add a download button for the generated PDF
+    with open(pdf_path, "rb") as pdf_file:
+        st.download_button(
+            label="",
+            icon=":material/download:",
+            data=pdf_file,
+            file_name=file_name,
+            mime="application/pdf",
+            key=f"download_{file_name}"
+        )
+
